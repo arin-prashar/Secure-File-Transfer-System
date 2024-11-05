172f33b1891e048c9227bbe913c57c08bc820405
arin-prashar <prashararin@gmail.com>
2024-11-05 20:29:00 +0530

    Server side And Receiver connection Final

--- sender/sender_main.py
+++ sender/sender_main.py
@@ -1,10 +1,13 @@
 import os
+import urllib
 import pathlib
 import sys
 import socket
 from dotenv import load_dotenv
 load_dotenv()
 
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 import sender.encrypt as encrypt
 import sender.fileInfo as fi
 
@@ -15,7 +18,7 @@ def fileSelect():
         os.listdir("Shared_Files")
     except FileNotFoundError:
         os.mkdir("Shared_Files")
-    
+
     Y_N=input("Is the file in the Shared Files Folder? (Y/N): ")
     if Y_N.lower() == "n":
         print("Please drop the file into the Shared Files Folder and rerun the program.")
@@ -26,16 +29,18 @@ def fileSelect():
     for i in range(len(files)):
         print(f"{i+1}. {files[i]}")
 
-
-
-
-
-
-
-
-
-
-
+def sendFile(user:str, file:str):
+    client=MongoClient(os.getenv("uri",server_api=ServerApi('1')))
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(os.getenv("port"),os.getenv("host"))
+    socket.connect((os.getenv("host"), os.getenv("port")))
+    print("Connected to the server.")
+    print("Sending file...")
+    file = fi.FileInfo(file)
+    file.encrypt()
+    file.send(socket)
+    print("File sent.")
+    socket.close()
 
 
 
@@ -53,4 +58,5 @@ def fileSelect():
 
 
 def main():
+    # client=MongoClient(os.getenv("uri",server_api=ServerApi('1')))
     fileSelect()
